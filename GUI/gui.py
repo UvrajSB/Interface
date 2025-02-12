0b8048b39e8bcbc0fa6a0c362790e72cb86e2ef7
UvrajSB <mryuvrajsbhadauria@gmail.com>
2025-02-12 06:24:58 +0530

    GUI Updates

---
+++ GUI/gui.py
@@ -0,0 +1,26 @@
+page = """
+<|layout|columns=250px 1|
+
+
+<|part|class_name=sidebar|
+
+
+# Interface
+
+**Select Mode**{: .color-primary}
+<|{value}|selector|lov=Cloud LLM; Secure Cloud LLM; On-Device LLM; Auto Prompt Routing|>
+<|Reset Conversation|button|class_name=fullwidth align-item-bottom plain|id=reset_app_button|on_action=reset_chat|>
+|>
+
+
+
+<|part|class_name=p2 align-item-bottom table|
+<|{conversation}|table|style=style_conv|show_all|selected={selected_row}|rebuild|>
+<|part|class_name=card mt1|
+<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|change_delay=-1|>
+|>
+|>
+
+
+|>
+"""
--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 import os
 import sys
-
 from taipy.gui import Gui, State, notify
-import openai
-
+import aisuite as ai
+import GUI.gui as gui
 from langchain_ollama import ChatOllama
-
 from dotenv import load_dotenv
 
 client = None
 context = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today? "
 conversation = {
-    "Conversation": ["Who are you?", "Hi! I am GPT-4. How can I help you today?"]
+    "Conversation": []
 }
 current_user_message = ""
 past_conversations = []
 selected_conv = None
 selected_row = [1]
-
-
-
+value = ["Cloud LLM", "On-Device LLM", "Auto Prompt Routing"]
+content = "/GUI/logo.png"
 
 def on_init(state: State) -> None:
-    """
-    Initialize the app.
-
-    Args:
-        - state: The current state of the app.
-    """
     state.context = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today? "
     state.conversation = {
-        "Conversation": ["Who are you?", "Hi! I am GPT-4. How can I help you today?"]
+        "Conversation": []
     }
     state.current_user_message = ""
     state.past_conversations = []
@@ -39,16 +30,6 @@ def on_init(state: State) -> None:
 
 
 def request(state: State, prompt: str) -> str:
-    """
-    Send a prompt to the GPT-4 API and return the response.
-
-    Args:
-        - state: The current state of the app.
-        - prompt: The prompt to send to the API.
-
-    Returns:
-        The response from the API.
-    """
     # response = state.client.chat.completions.create(
     #     messages=[
     #         {
@@ -59,31 +40,38 @@ def request(state: State, prompt: str) -> str:
     #     model="gpt-4-turbo-preview",
     # )
     # return response.choices[0].message.content
-    response = chat_model.invoke(prompt)
-    print(response.content)
-    return response.content
+    
+    #Offline response
+    #response = chat_model.invoke(prompt).content
+
+    messages = [
+        {"role": "system", "content": "Respond in Pirate English."},
+        {"role": "user", "content": "Tell me a joke."},
+    ]
+    
+    message = {
+        "role": "user",
+        "content": prompt,
+    }
+    
+    model = "google:"
+
+    response = client.chat.completions.create(
+        model=model,
+        message=message,
+        temperature=0.75
+    )
+    return response.choices[0].message.content
 
 def update_context(state: State) -> None:
-    """
-    Update the context with the user's message and the AI's response.
-
-    Args:
-        - state: The current state of the app.
-    """
     state.context += f"Human: \n {state.current_user_message}\n\n AI:"
-    answer = request(state, state.context).replace("\n", "")
+    answer = request(state, state.context)
     state.context += answer
     state.selected_row = [len(state.conversation["Conversation"]) + 1]
     return answer
 
 
 def send_message(state: State) -> None:
-    """
-    Send the user's message to the API and update the context.
-
-    Args:
-        - state: The current state of the app.
-    """
     notify(state, "info", "Sending message...")
     answer = update_context(state)
     conv = state.conversation._dict.copy()
@@ -94,17 +82,6 @@ def send_message(state: State) -> None:
 
 
 def style_conv(state: State, idx: int, row: int) -> str:
-    """
-    Apply a style to the conversation table depending on the message's author.
-
-    Args:
-        - state: The current state of the app.
-        - idx: The index of the message in the table.
-        - row: The row of the message in the table.
-
-    Returns:
-        The style to apply to the message.
-    """
     if idx is None:
         return None
     elif idx % 2 == 0:
@@ -114,89 +91,31 @@ def style_conv(state: State, idx: int, row: int) -> str:
 
 
 def on_exception(state, function_name: str, ex: Exception) -> None:
-    """
-    Catches exceptions and notifies user in Taipy GUI
-
-    Args:
-        state (State): Taipy GUI state
-        function_name (str): Name of function where exception occured
-        ex (Exception): Exception
-    """
     notify(state, "error", f"An error occured in {function_name}: {ex}")
 
 
 def reset_chat(state: State) -> None:
-    """
-    Reset the chat by clearing the conversation.
-
-    Args:
-        - state: The current state of the app.
-    """
     state.past_conversations = state.past_conversations + [
         [len(state.past_conversations), state.conversation]
     ]
     state.conversation = {
-        "Conversation": ["Who are you?", "Hi! I am GPT-4. How can I help you today?"]
+        "Conversation": [""]
     }
 
 
 def tree_adapter(item: list) -> [str, str]:
-    """
-    Converts element of past_conversations to id and displayed string
-
-    Args:
-        item: element of past_conversations
-
-    Returns:
-        id and displayed string
-    """
     identifier = item[0]
     if len(item[1]["Conversation"]) > 3:
         return (identifier, item[1]["Conversation"][2][:50] + "...")
     return (item[0], "Empty conversation")
 
-
-def select_conv(state: State, var_name: str, value) -> None:
-    """
-    Selects conversation from past_conversations
-
-    Args:
-        state: The current state of the app.
-        var_name: "selected_conv"
-        value: [[id, conversation]]
-    """
-    state.conversation = state.past_conversations[value[0][0]][1]
-    state.context = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today? "
-    for i in range(2, len(state.conversation["Conversation"]), 2):
-        state.context += f"Human: \n {state.conversation['Conversation'][i]}\n\n AI:"
-        state.context += state.conversation["Conversation"][i + 1]
-    state.selected_row = [len(state.conversation["Conversation"]) + 1]
-
-
 past_prompts = []
-
-page = """
-<|layout|columns=300px 1|
-<|part|class_name=sidebar|
-# Interface # {: .logo-text}
-<|New Conversation|button|class_name=fullwidth plain|id=reset_app_button|on_action=reset_chat|>
-### Previous activities ### {: .h5 .mt2 .mb-half}
-<|{selected_conv}|tree|lov={past_conversations}|class_name=past_prompts_list|multiple|adapter=tree_adapter|on_change=select_conv|>
-|>
-
-<|part|class_name=p2 align-item-bottom table|
-<|{conversation}|table|style=style_conv|show_all|selected={selected_row}|rebuild|>
-<|part|class_name=card mt1|
-<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|change_delay=-1|>
-|>
-|>
-|>
-"""
 
 if __name__ == "__main__":
     load_dotenv()
 
     #client = openai.Client(api_key=os.getenv("OPENAI_API_KEY"))
+    LLM_client = ai.Client()
     #local inferencing
-    chat_model = ChatOllama(model="llama3.1:8b")
-    Gui(page).run(debug=True, dark_mode=True, use_reloader=True, title="Interface")
+    #chat_model = ChatOllama(model="deepseek-r1:1.5b")
+    Gui(gui.page).run(debug=True, use_reloader=True ,title="Interface")
